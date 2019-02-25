73d1a1bcb5cefe7293ddbc5a8e25b1a53cd92e30
jianch3nq <38032294+jianch3nq@users.noreply.github.com>
2019-02-25 11:21:05 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,13 @@
+<div id = 'header'>
+    <ul id="menu-bar">
+        <li > <a href ='index.php'>Home</a></li>
+        <li> <a href ='#'>Attractions</a>
+            <ul>
+                <li><a href = 'attractions.php'>Category A</a></li>
+            </ul>
+        </li>
+        <li> <a href ='itinerary.php'>Plan Itinerary</a></li>
+    </ul>
+</div>
+</div>
+
