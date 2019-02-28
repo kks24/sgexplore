0f7931d9fa898f39f3a267b86b55f354ac779c22
jianch3nq <38032294+jianch3nq@users.noreply.github.com>
2019-03-01 01:49:20 +0800

    Doing up the necessary changes in validating

--- itinerary.php
+++ itinerary.php
@@ -9,6 +9,31 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>SGXplore</title>
         <link rel="stylesheet" type="text/css" href="SGXplore.css">
+        <script type="text/javascript">
+
+            function dateChanged() {
+                if (document.getElementById("date").value != null) {
+                    for (i = 0; i < document.getElementsByName("search").length; i++) {
+                        document.getElementsByName("search")[i].disabled = false;
+                    }
+                    document.getElementsByName("search")[0].checked = true;
+                    document.getElementById("selectCat").disabled = true;
+                    document.getElementById("current").append(document.getElementById("date").value);
+                }
+            }
+
+
+            function placeSelected() {
+                document.getElementById("selectCat").disabled = true;
+                document.getElementById("keyword").disabled = false;
+            }
+
+            function catSelected() {
+
+                document.getElementById("selectCat").disabled = false;
+                document.getElementById("keyword").disabled = true;
+            }
+        </script>
     </head>
     <body>
         <?php
@@ -18,32 +43,30 @@ and open the template in the editor.
             <h2>Plan itinerary</h2>
             <table>
                 <tr>
-                    <td>Select date of visiting: <input type ="date" size="50"></td>
+                    <td>Select date of visiting: <input type ="date" id="date"  onchange="dateChanged()" size="50"></td>
                     <td>Expected Weather: <br></td>
                 </tr>
                 <tr>
-                    <td><input type="radio" name="search" value="male" checked="check"> Search for Place: 
-                        <input type ="search" size="18">
+                    <td><input  type="radio" name="search" value="place" onchange="placeSelected()" disabled> Search for Place: 
+                        <input type ="search" id="keyword" size="18" disabled>
                     </td>
-                    <td> <input type="radio" name="search" value="male"> Select Category: 
-                        <select name="category"> 
-                            <option value="A">Category C</option>
-                            <option value="A">Category B</option>
-                        </select> <br></td>
+                    <td> <input type="radio" name="search" value="category" onchange="catSelected()" disabled> Select Category: 
+                        <select name="category" id ="selectCat" disabled> 
+                            <option value="A">Category A</option>
+                            <option value="B">Category B</option>
+                        </select> <br>
+                    </td>
                 </tr>
             </table>
 
+            <div id="results">
 
-
-
-
-
-
-
-            <br>
+            </div>
         </div>
+
         <div id ="current">
-            Current Itinerary
+            Current Itinerary:<br>
+            
         </div>
 
     </body>
